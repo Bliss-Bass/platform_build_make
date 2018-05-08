082f19f9feeea8bc30481e5c284bc28c68b1f13b
aclegg2011 <32418121+aclegg2011@users.noreply.github.com>
2018-05-08 15:47:25 -0700

    add build.sh to build_make

---
+++ core/scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rom_fp="$(date +%y%m%d)"
+mkdir -p release/$rom_fp/
+set -e
+
+(cd device/phh/treble; bash generate.sh)
+
+export ANDROID_JACK_VM_ARGS="-Xmx8g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
+
+. build/envsetup.sh
+
+buildVariant() {
+        lunch $1
+        make BUILD_NUMBER=$rom_fp installclean
+        make BUILD_NUMBER=$rom_fp -j32 systemimage
+        make BUILD_NUMBER=$rom_fp vndk-test-sepolicy
+        cp $OUT/system.img release/$rom_fp/system-${2}.img
+}
+
+#buildVariant treble_arm64_avN-userdebug arm64-aonly-vanilla-nosu
+buildVariant treble_arm64_agS-userdebug arm64-aonly-gapps-su
+
+#buildVariant treble_arm64_bvN-userdebug arm64-ab-vanilla-nosu
+#buildVariant treble_arm64_bgS-userdebug arm64-ab-gapps-su
+
+#buildVariant treble_arm_avN-userdebug arm-aonly-vanilla-nosu
